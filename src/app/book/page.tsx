2b419635dbec73c6e1fe7eb62c88aabcd8b8e56d
Veeravel-S <veeravel.igt@gmail.com>
2026-07-29 18:23:56 +0530

    changes

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import UTMTracker from '@/components/ui/UTMTracker';
 import { PsychologyMedicalPracticeLanding } from '@/components/sections/PsychologyMedicalPractice';
 
@@ -11,6 +12,33 @@ export const metadata: Metadata = {
 export default function PsychologyOfMedicalPracticePage() {
   return (
     <main>
+      <Script
+        id="meta-pixel-book"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '27396972110003418');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=27396972110003418&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
       <UTMTracker />
       <PsychologyMedicalPracticeLanding />
     </main>
