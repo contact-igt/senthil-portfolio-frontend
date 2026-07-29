964b85228d0be76c9ef525bd583dbfd87d0a2d1b
Veeravel-S <veeravel.igt@gmail.com>
2026-07-29 18:38:36 +0530

    changes

--- src/app/book/page.tsx
+++ src/app/book/page.tsx
@@ -13,6 +13,19 @@ export default function PsychologyOfMedicalPracticePage() {
   return (
     <main>
       <Script
+        id="microsoft-clarity-book"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xu1h6ut0j1");
+          `,
+        }}
+      />
+      <Script
         id="meta-pixel-book"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
